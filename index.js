9a053ea61fc59937a7befe54f19936969f685f53
Cory Wang <cory.wang11@gmail.com>
2020-07-15 20:56:36 -0700

    worked out the JS and finished functionality

--- index.js
+++ index.js
@@ -1,95 +1,222 @@
+/* Setting global counters */
+let currentQuestion = 0;
+let score = 0;
+
 function startQuiz() {
     $('#begin').on('click', function(event) {
+        /* Shuffle the STORE array */
+        randomize();
+        /* Render the next question in the STORE array */
         showQuestion();
+        /* Render the score and question number counters */
+        $('.stats').show();
     });
+}
+
+// function randomize() {
+
+//     shuffle(STORE);
+//     console.log(STORE);
+
+// }
+
+/* Render the start screen */
+function renderFirst() {
+
+    $('.stats').hide();
+
+    $('main').html(
+        `
+        <fieldset>
+           <div class='textc'>
+               <h2>Are You Smarter than a Tech Entreprenuer?</h2>
+               <p>This test is designed for you to have fun and learn a thing or two that will help you start your own company.</p>
+                <img src='images/thinking_elon.jpg' alt='Thinking Entrepreneur'>
+           </div>
+           <button type='button' id="begin">Start the Show</button>
+        </fieldset>
+        `
+    );
+    startQuiz();
 }
 
 /* Update Question # and # of questions answered correctly */
 function currentStats() {
-    let currentQuestion = STORE.currentQuestion;
-    let numCorrect = STORE.score;
-
     $('.stats').html(
-        `<p> Question #: ${currentQuestion}  |   Score: ${numCorrect} </p>`
+        `<div class='stats'>
+        <ul>
+            <li>
+                Question:
+                <span class='question-number'>${currentQuestion+1}</span>/${STORE.length}
+            </li>
+            <li>
+                Score:
+                <span class='score'>${score}</span>
+            </li>
+        </div>`
     );
 }
 
 /* Need a way to show the answers to a question */
 function showAnswers() {
-    let numQuestion = STORE.questions[STORE.currentQuestion];
     /* Iterate through the answers to a question */
-    for (let i = 0; i < numQuestion.answers.length; i++) {
-        $('.js-questions').append(
-            `<input type='radio' name='choice' id='option${i+1}' value='${numQuestion.answers[i]}' tabindex = ${i+1}>
-            <label for="option${i+1}">${numQuestion.answers[i]}</label>`
+    for (let i = 0; i < STORE[currentQuestion].answers.length; i++) {
+        $('.js-answers').append(
+            `<input type='radio' name='choice' id='option${i+1}' value="${STORE[currentQuestion].answers[i]}" tabindex = ${i+1}>
+            <label for="option${i+1}">${STORE[currentQuestion].answers[i]}</label>
+            <br>`
         );
     };
 }
 
 /* Adding the question + answers to the DOM */
 function showQuestion() {
-    let questionObject = STORE.questions(STORE.currentQuestion);
+    if ((currentQuestion) === STORE.length) {
+        
+        finalScreen();
 
-    currentStats();
-    /* Add the current question html to the DOM */
-    $('main').html(
-        `<fieldset>
-           <div class='textc'>
-               <h2>Question ${STORE.currentQuestion}</h2>
-               <p>${questionObject.question}</p>
-           </div>
-           <div class="choice">
-               <form id='js-questions' class='questions'>
-               </form>
-           </div>
-           <button type='button' id="submit-question">Submit Answer</button>
-        </fieldset>`
+    } else {
+
+        /* Add the current question html to the DOM */
+        $('main').html(
+            `<form id='js-questions' class='questions'>
+                <fieldset>
+                    <div class='textc'>
+                        <h2>Question ${currentQuestion+1}</h2>
+                        <p>${STORE[currentQuestion].question}</p>
+                    </div>
+                    <div class="js-answers">
+                    </div>
+                <button type='submit' id="submit-question">Submit Answer</button>
+                </fieldset>
+            </form>`
     );
+    /* Add the answers to the question to the DOM */
     showAnswers();
+    }
+}
+
+function resetStats() {
+ 
+    currentQuestion = 0;
+    score = 0;
+    $('.score').text(0);
+    $('.questionNumber').text(0);
+    currentStats();
+
 }
 
 function nextQuestion() {
-    /*
-    On clicking next question
-    Find the next question in the STORE.question array
-    render the question and answers
-    */
-
+    /* On clicking next question */
+    $('#continue').on('click', function(event){
+        currentQuestion++;
+        /* Render the question and answers */
+        showQuestion();
+        currentStats();
+    });    
 }
 
+
 function questionSubmit() {
-    /*
-    On submitting an answer to a question
-    */
-    $('#submit-question').on('click', '')
-    /*Check if the answer is right
-    Render correct answer content if right, wrong answer content if not
-    */
+    /*On submitting an answer to a question*/
+    $('#container').on('submit', '#js-questions', function(event){
+        event.preventDefault();
+        /* Check if the answer is right*/
+        let selected = $('input:checked');
+        let answer = selected.val();
+        let correct = STORE[currentQuestion].correctAnswer;
+        console.log(answer, 'correctAnswer');
+        console.log(correct, 'answer');
+        if (answer === correct) {
+            score = score+1;
+            /*Render correct answer content if right, wrong answer content if not*/
+            $('main').html(
+                `
+                <div class='container'>
+                <fieldset>
+                    <div class='textc'>
+                        <h2>Correct!</h2>
+                        <img src='images/happy_elon.jpg' alt='Happy Entrepreneur'>
+                        <p>${STORE[currentQuestion].funFact}</p>
+                    </div>
+                <button type='button' id='continue'>Next Question</button>
+                </fieldset>
+                </div>
+                `
+            );
+        } else {
+            $('main').html(
+                `
+                <fieldset>
+                    <div class='textc'>
+                        <h2>Incorrect</h2>
+                        <img src='images/unhappy_elon.jpg' alt='Unhappy Entrepreneur'>
+                        <p>The right answer was ${STORE[currentQuestion].correctAnswer}!</p><br>
+                        <p>${STORE[currentQuestion].funFact}</p>
+                    </div>
+                <button type='submit' id="continue">Next Question</button>
+                </fieldset>
+                `
+            );
+        }
+        currentStats();
+        /* Setting event listener */
+        nextQuestion();
+    });
 }
 
 function restartQuiz() {
-    /* 
-    Render as part of the final screen
-    Click the restart button to reset back to DOM original status
-    use $().html
-    */
+    $('#container').on('click', '#restart', function(event){
+        $('main').html(
+            `
+            <fieldset>
+            <div class='textc'>
+                <h2>Are You Smarter than a Tech Entreprenuer?</h2>
+                <p>This test is designed for you to have fun and learn a thing or two that will help you start your own company.</p>
+                    <img src='images/thinking_elon.jpg' alt='Thinking Entrepreneur'>
+            </div>
+            <button type='button' id="begin">Start the Show</button>
+            </fieldset>
+            `
+        )
+        resetStats();
+        /* Setting event listener */
+        startQuiz();
+    });
 }
 
 function finalScreen() {
-    /*
-    On clicking the next button
-    After the final question is answered
-    Show the final score and a good/average/bad descriptor
-    load the restart quiz function
-    */
+    /* Show the final score and a good/average/bad descriptor */
+    $('.stats').hide();
+    $('main').html(
+        `
+        <fieldset>
+           <div class='textc'>
+               <h2>You got ${score}/${STORE.length} right!</h2>
+               <p>Elon would be proud of you!</p>
+               <img src="images/dancing_elon.gif" alt="entrepreneur celebration" style="width:240px; height:175px">
+           </div>
+           <button type='button' id="restart">Restart</button>
+        </fieldset>
+        `
+    );
+    /* Setting event listener */
+    restartQuiz();
 }
 
 
 function startTheShow() {
-   startQuiz();
-   currentStats();
-   showAnswers();
-   showQuestion();
+    // randomize();
+    showAnswers();
+    resetStats();
+    finalScreen();
+    showQuestion();
+    currentStats();
+    startQuiz();
+    questionSubmit();
+    nextQuestion();
+    restartQuiz();
+    renderFirst();
 }
 
 $(startTheShow);
--- store.js
+++ store.js
@@ -1,137 +1,133 @@
-const STORE {
-    questions: [
+const STORE = [
         /* 1 */
-        {
-            id: 1,
-            question: 'What is the best state to incorporate in?',
-            answers: [
-                'Whatever state your company is based in',
-                'Delaware',
-                'Texas',
-                'Wyoming'
-            ],
-            correctAnswer: 'Delaware'
-            funFact:'Many sophisticated investors will use this as a litmus test. Most think that people incorporate in Delaware for tax reasons, but other states offer similar or better tax breaks. Knowledgeable founders choose Delaware is the qualities for the courts and speed in which they process legal documents/requests (like incorporating a company) and the second is the existance of the Court of Chancery to rule on corporate legal disputs efficiently and quickly.'
-        },
-        /* 2 */
-        {
-            id: 2,
-            question: 'What is a prenciple of lean customer development?',
-            answers: [
-                'Build it, and they will come',
-                'The customer knows best',
-                'Why wait for people to come when you can pay them to care',
-                'It’s better to have 100 people who love you, than 10,000 who are apathetic'
-            ],
-            correctAnswer: "It’s better to have 100 people who love you, than 10,000 who don't",
-            funFact:'Early on, when founding a company, its easy to want thousands of people on your platform as quickly as possible, but that creates two problems: user retention and scaling your infrastructure. Ideally, you want a core group of users who are willing to give you plenty of feedback and rely on your product/services like a basic utility.'
-        },
-        /* 3 */
-        {
-            id: 3,
-            question: 'What is a FAST agreement?',
-            answers: [
-                'Final Administrative Simplified Term',
-                'First Anchor Stock Turnaround',
-                'Founder Advisor Standard Template',
-                'First Arbitration Signing Template'
-            ],
-            correctAnswer: 'Founder Advisor Standard Template'
-            funFact:'The Founder Advisor Standard Template agreement is used to formally engage advisors by compensating them with equity to establish productive working relationships, trading advice, and support for a standardized amount of equity.'
-        },
-        /* 4 */
-        {
-            id: 4,
-            question: 'What do you need to file within 30 days of issuing founder shares to save a boat-load of money?',
-            answers: [
-                '1099B',
-                '8949',
-                '83(b) Election',
-                'Schedule D'
-            ]
-            correctAnswer: '83(b) Election'
-            funFact:"If a founder does not file their 83(b) election within the 30 day time period, then they could end up paying hundreds of thousands of dollars for their shares instead of pennies on the dollar. The election gives the startup founder the option to pay taxes on the fair market value of restricted stock AT THE TIME of granting (close to 0) rather than later on once funding has been raised and the market capitalization of the company has been established (shares tax worth up to hundreds of thousands of dollars)."
-        },
-        /* 5 */
-        {
-            id: 5,
-            question: 'What is the difference between a Series A and Seed round?',
-            answers: [
-                "Basically nothing (in today's market)",
-                'How much money you have raised',
-                'How much money you can raise',
-                'What kind of investors will participate'
-            ],
-            correctAnswer: 'How much money you can raise'
-            funFact:"Angel investors that're currently active will tell you that the rules have been of the window for the past several years [2016-2019]. Typically seed rounds are less than 2 Million [MM] and series A can be up to 10 MM, but the rise of supergiant seed rounds (5 MM or greater) shows that early stage investment is being concentrated into the winners."
-        },
-        /* 6  */
-        {
-            id: 6,
-            question: 'What character archetype makes for a good Founder?',
-            answers: [
-                'A Greater Fool',
-                'Type-A Leader',
-                'Hustler',
-                'The Coding Guru'
-            ]
-            correctAnswer: 'A Greater Fool'
-            funFact:"A founder needs to believe they can succeed where others have failed. In other words, they need to be just a little crazy with the wherewithal to follow through. Other personality types are important for early team members, so just because you aren't a greater fool doesn't mean you can't create something incredible."
-        },
-        /* 7 */
-        {
-            id: 7,
-            question: 'What is “paper prototyping"?',
-            answers: [
-                'A physical prototype, made out of corrugated double-walled cardboard or “paper”',
-                'A wire-framing technique to simulate a user’s experience through an application for random people on a piece of paper',
-                'The MVP or minimum viable prototype strategy that will allow you to have paying customers',
-                'When you decide on important product decisions through a paper plane flying competition'
-            ]
-            correctAnswer: 'A wire-framing technique to simulate a user’s experience through an application on a piece of paper'
-            funFact:"<a href='http://www.jamieshiller.com/paper-prototyping-an-ipad-app-business-model-generation/'>Paper prototypes</a> are used when trying to determine how a minimum viable product (MVP) or new feature will look from the user's perspective. The key is having a test s who isn't involved with the project at all, so they don't know what it's supposed to look like."
-        },
-        /* 8 */
-        {
-            id: 8,
-            question: 'What makes for a good company name?',
-            answers: [
-                'Something that you paid a branding consultant thousands of dollars for',
-                'A Portmanteau - Two words combined into one i.e. facebook',
-                'Something short and easy to spell based on pronunciation',
-                'Something that has an available domain'
-            ]
-            correctAnswer: 'Something short and easy to spell based on pronunciation'
-            funFact:"It's important when choosing a name that people remember. They will likely google your company almost immediately after hearing it and probably wont have you eplaining that the 'y's are actually 'i's."
-        },
-        /* 9 */
-        {
-            id: 9,
-            question: 'What is the reason that most startups fail?',
-            answers: [
-                'Product/Market Fit',
-                'Co-founder disagreements',
-                'Running out of money through lack of funding',
-                'Legal disputes'
-            ]
-            correctAnswer: 'Co-founder disagreements'
-            funFact:"While all of the above are valid reasons for a venture failing, co-founder falling-outs are what kill a business idea more often than not. During the beginning of a company's life cycle, the biggest resource is human capital. If that's compromised, then so are your options."
-        },
-        /* 10 */
-        {
-            id: 10,
-            question: 'What is stopping you from building a business?',
-            answers: [
-                'Nothing',
-                'Everything',
-                'Most things',
-                'Having a life'
-            ]
-            correctAnswer: 'Nothing'
-            funFact:"While not everyone would enjoy starting a venture-bacled business, small businesses account for a large percentage of economies around the world and are what drive innovation. With the age of cloud technology, everyone has an opportunity to create something on a global scale."
-        }
-    ];
-    /* Global Counters */
-    currentQuestion: 0;
-    score: 0
-}
+    {
+        id: 1,
+        question: 'What is the best state to incorporate in?',
+        answers: [
+            'Whatever state your company is based in',
+            'Delaware',
+            'Texas',
+            'Wyoming'
+        ],
+        correctAnswer: 'Delaware',
+        funFact:'Sophisticated investors will use this as a litmus test. People think that founders incorporate in Delaware for tax reasons, but other states offer similar or better tax breaks. Knowledgeable founders choose Delaware for the quality of their courts and speed in which they process legal documents/requests (like forming a corporation) and the second is the existance of the Court of Chancery to rule on corporate legal disputs efficiently and quickly.'
+    },
+    /* 2 */
+    {
+        id: 2,
+        question: 'What is a principle of lean customer development?',
+        answers: [
+            'Build it, and they will come',
+            'The customer knows best',
+            'Why wait for people to come when you can pay them to care',
+            "It is better to have 100 people who love you, than 10,000 who do not"
+        ],
+        correctAnswer: "It is better to have 100 people who love you, than 10,000 who do not",
+        funFact:'Early on, when founding a company, its easy to want thousands of people on your platform as quickly as possible, but that creates two problems: user retention and scaling your infrastructure. Ideally, you want a core group of users who are willing to give you plenty of feedback and rely on your product/services like a basic utility.'
+    },
+    /* 3 */
+    {
+        id: 3,
+        question: 'What is a FAST agreement?',
+        answers: [
+            'Final Administrative Simplified Term',
+            'First Anchor Stock Turnaround',
+            'Founder Advisor Standard Template',
+            'First Arbitration Signing Template'
+        ],
+        correctAnswer: 'Founder Advisor Standard Template',
+        funFact:'The Founder Advisor Standard Template agreement is used to formally engage advisors by compensating them with equity to establish productive working relationships, trading advice, and support for a standardized amount of equity.'
+    },
+    /* 4 */
+    {
+        id: 4,
+        question: 'What do you need to file within 30 days of issuing founder shares to save a boat-load of money?',
+        answers: [
+            '1099B',
+            '8949',
+            '83(b) Election',
+            'Schedule D'
+        ],
+        correctAnswer: '83(b) Election',
+        funFact:"If a founder does not file their 83(b) election within the 30 day time period, then they could end up paying hundreds of thousands of dollars for their shares instead of pennies on the dollar. The election gives the startup founder the option to pay taxes on the fair market value of restricted stock AT THE TIME of granting (close to 0) rather than later on once funding has been raised and the market capitalization of the company has been established (shares tax worth up to hundreds of thousands of dollars)."
+    },
+    /* 5 */
+    {
+        id: 5,
+        question: 'What is the difference between a Series A and Seed round?',
+        answers: [
+            "Basically nothing (in today's market)",
+            'How much money you have raised',
+            'How much money you can raise',
+            'What kind of investors will participate'
+        ],
+        correctAnswer: 'How much money you can raise',
+        funFact:"Angel investors that're currently active will tell you that the rules have been of the window for the past several years [2016-2019]. Typically seed rounds are less than 2 Million [MM] and series A can be up to 10 MM, but the rise of supergiant seed rounds (5 MM or greater) shows that early stage investment is being concentrated into winners."
+    },
+    /* 6  */
+    {
+        id: 6,
+        question: 'What character archetype makes for a good Founder?',
+        answers: [
+            'A Greater Fool',
+            'Type-A Leader',
+            'Hustler',
+            'The Coding Guru'
+        ],
+        correctAnswer: 'A Greater Fool',
+        funFact:"A founder needs to believe they can succeed where others have failed. In other words, they need to be just a little crazy with the wherewithal to follow through. Other personality types are important for early team members so just because you aren't a greater fool doesn't mean you can't create something incredible."
+    },
+    /* 7 */
+    {
+        id: 7,
+        question: 'What is “paper prototyping"?',
+        answers: [
+            'A physical prototype, made out of corrugated double-walled cardboard or “paper”',
+            'A wire-framing technique to simulate the user experience through an application for random people on a piece of paper',
+            'The MVP or minimum viable prototype strategy that will allow you to have paying customers',
+            'When you decide on important product decisions through a paper plane flying competition'
+        ],
+        correctAnswer: 'A wire-framing technique to simulate the user experience through an application for random people on a piece of paper',
+        funFact:"<a href='http://www.jamieshiller.com/paper-prototyping-an-ipad-app-business-model-generation/'>Paper prototypes</a> are used when trying to determine how a minimum viable product (MVP) or new feature will look from the user's perspective. The key is having a test s who isn't involved with the project at all so they don't know what it's supposed to look like."
+    },
+    /* 8 */
+    {
+        id: 8,
+        question: 'What makes for a good company name?',
+        answers: [
+            'Something that you paid a branding consultant thousands of dollars for',
+            'A Portmanteau - Two words combined into one i.e. facebook',
+            'Something short and easy to spell based on pronunciation',
+            'Something that has an available domain'
+        ],
+        correctAnswer: 'Something short and easy to spell based on pronunciation',
+        funFact:"It's important when choosing a name that people remember. They will likely google your company almost immediately after hearing it and probably wont have you explaining that the 'y's are actually 'i's."
+    },
+    /* 9 */
+    {
+        id: 9,
+        question: 'What is the reason that most startups fail?',
+        answers: [
+            'Product/Market Fit',
+            'Co-founder disagreements',
+            'Running out of money through lack of funding',
+            'Legal disputes'
+        ],
+        correctAnswer: 'Co-founder disagreements',
+        funFact:"While all of the above are valid reasons for a venture failing, co-founder falling-outs are what kill a business idea more often than not. During the beginning of a company's life cycle the biggest resource is human capital. If that's compromised then so are your options."
+    },
+    /* 10 */
+    {
+        id: 10,
+        question: 'What is stopping you from building a business?',
+        answers: [
+            'Nothing',
+            'Everything',
+            'Most things',
+            'Having a life'
+        ],
+        correctAnswer: 'Nothing',
+        funFact:"While not everyone would enjoy starting a venture-bacled business, small businesses account for a large percentage of economies around the world and are what drive innovation. With the age of cloud technology, everyone has an opportunity to create something on a global scale."
+    }
+];
+
